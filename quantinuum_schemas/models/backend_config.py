d06f16606460f53e067008b845ce72ba622bb1ed
isobelhooper <isobel.hooper@cambridgequantum.com>
2025-05-06 12:06:02 +0100

    feat: Add optional max_cost field to QuantinuumConfig (#16)

--- quantinuum_schemas/models/backend_config.py
+++ quantinuum_schemas/models/backend_config.py
@@ -182,6 +182,7 @@ class QuantinuumConfig(BaseBackendConfig):
     allow_2q_gate_rebase: bool = False
     leakage_detection: bool = False
     simplify_initial: bool = False
+    max_cost: Optional[int] = None
     error_params: Optional[UserErrorParams] = None
 
 
