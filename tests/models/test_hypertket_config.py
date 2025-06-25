191cd973a2a9d18f7e5efca1340c6f6b03537474
Ariana Hlavaty <ariana.hlavaty@quantinuum.com>
2025-06-25 09:22:42 +0100

    Add test

--- tests/models/test_hypertket_config.py
+++ tests/models/test_hypertket_config.py
@@ -1,8 +1,22 @@
 """Test hypertket config model."""
 
-from quantinuum_schemas.models.hypertket_config import HyperTketConfig
+from quantinuum_schemas.models.hypertket_config import (
+    DualStrat,
+    HyperTketConfig,
+    QubitReuseConfig,
+)
 
 
 def test_instantiate() -> None:
     """Test instantiation of HyperTketConfig."""
     HyperTketConfig()
+
+
+def test_enum_serialization() -> None:
+    """Test serialization of DualStrat."""
+    config = HyperTketConfig(
+        qubit_reuse_config=QubitReuseConfig(dual_circuit_strategy=DualStrat.AUTO)
+    )
+
+    serialised = config.to_serializable()
+    HyperTketConfig.from_serializable(serialised)
